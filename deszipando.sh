fd575bd9f512ab529f2148c82e033d60bab6eeba
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-14 05:00:01 -0300

    Update deszipando.sh

--- deszipando.sh
+++ deszipando.sh
@@ -1,6 +1,1 @@
-unzip brplay.zip -d instance2359
-
-cd instance000
-mv judge0-v1.13.0/docker-compose.yml judge0-v1.13.0/judge0.conf .
-rm -rf judge0-v1.13.0
-cd ..
+unzip brplay.zip -o -d instance2359
